24d6ec24a964a53540fad1247b77fa1699ecfbcd
杨活龙 <yanghuolong@zhisland.com>
2018-03-28 15:14:37 +0800

    test goto

---
+++ gototest.go
@@ -0,0 +1,14 @@
+package main
+
+import (
+	"fmt"
+)
+
+func main() {
+end:
+	fmt.Println("111111111111111111")
+	goto end
+	fmt.Println("22222222222222")
+	fmt.Println("333333333333")
+	fmt.Println("44444444444")
+}
